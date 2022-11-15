792745416fcb8acd3b6511a7f42a23cbd1696b38
kyoheikishima <56190171+kyoheikishima@users.noreply.github.com>
2022-11-15 09:56:42 +0900

    1_project create

---
+++ japanese_calendar/src/main/java/com/example/japanese_calendar/JapaneseCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.japanese_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseCalendarApplication.class, args);
+	}
+
+}
---
+++ japanese_calendar/src/test/java/com/example/japanese_calendar/JapaneseCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.japanese_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
