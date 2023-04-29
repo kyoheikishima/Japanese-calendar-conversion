59f776be013eafc69ac9622f3259acd1f47d14d9
kyoheikishima <56190171+kyoheikishima@users.noreply.github.com>
2023-04-30 05:15:50 +0900

    研修用

--- japanese_calendar/src/main/java/com/example/japanese_calendar/JapaneseCalendarApplication.java
+++ japanese_calendar/src/main/java/com/example/japanese_calendar/JapaneseCalendarApplication.java
@@ -1,13 +1,17 @@
 package com.example.japanese_calendar;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class JapaneseCalendarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JapaneseCalendarApplication.class, args);
+		double taxRate = 0.1;
+		JapaneseCalendarApplication j = new JapaneseCalendarApplication();
+		System.out.println(1500 + j.tax(1500, taxRate));
+
+		
+	}
+
+	protected double tax(double price, double consumptionTax) {
+		return price * consumptionTax;
 	}
 
 }
